8e79bdcce1119d74d9bf52fecbf5ca749ac8a2ba
jakob-ra <jakob.rauch@student.uva.nl>
2022-06-14 11:57:12 +0200

    requirements

---
+++ main.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import os
+import numpy as np
+import boto3
+from warcio.archiveiterator import ArchiveIterator
+import swifter
+import timeit
+import time
+from io import BytesIO
+from bs4 import BeautifulSoup, SoupStrainer
+import lxml
+import cchardet
+import botocore
+
+BeautifulSoup(markup, "lxml")
+lxml.etree.fromstring(markup)
+cchardet.detect(markup)
+botocore.client.Client(service_name='s3', region_name='us-east-1')
+BytesIO(markup)
+time
+timeit
+swifter = swifter.Swifter(pd.DataFrame())
+ArchiveIterator(BytesIO(markup))
+boto3.client('s3', region_name='us-east-1')
+os.getcwd()
+np.random.randn(10)
