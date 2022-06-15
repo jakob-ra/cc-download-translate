606f33b82b993f71429c2107688b8eccd97f77ae
jakob-ra <jakob.rauch@student.uva.nl>
2022-06-15 16:44:15 +0200

    athena pipeline

--- athena_lookup.py
+++ athena_lookup.py
@@ -1,128 +1,236 @@
-# first, create an IAM role with full access to S3, Athena, and EC2/Batch
-# Then, configure the session with your AWS credentials by running "aws configure" in your terminal
-import boto3
-import athena_query
+import time
+import pandas as pd
+import sys
 
-# params
-s3path_url_list = 's3://cc-extract/dataprovider_all_months/' # folder where url list is stored, needs to be without 'www.'
-output_bucket = 'cc-extract' # bucket to store the results
-output_path = 'test' # path in output_bucket to store the results
-params = {
-    'region': 'us-east-1',
-    'catalog': 'AwsDataCatalog',
-    'database': 'ccindex',
-    'bucket': output_bucket,
-    'path': output_path
-}
+class Athena_lookup():
+    def __init__(self, session, aws_params, s3path_url_list, crawl, n_subpages, url_keywords):
+        self.athena_client = session.client('athena')
+        self.s3_client = session.client('s3')
+        self.aws_params = aws_params
+        self.s3path_url_list = s3path_url_list
+        self.crawl = crawl
+        self.n_subpages = n_subpages
+        self.url_keywords = url_keywords
 
-# start up session
-session = boto3.Session()
-client = session.client('athena')
+    @staticmethod
+    def get_var_char_values(d):
+        return [obj['VarCharValue'] if len(obj) > 0 else None for obj in d['Data']]
 
-def create_url_list_table(client, params):
-    params['query'] = f"""CREATE EXTERNAL TABLE IF NOT EXISTS url_list (
-    websiteaddress               STRING, 
-    bvdidnumber                  STRING)
-    ROW FORMAT DELIMITED
-    FIELDS TERMINATED BY ','
-    LINES TERMINATED BY '\n'
-    LOCATION '{s3path_url_list}'
-    TBLPROPERTIES ('skip.header.line.count' = '1')
-    ;"""
-    athena_query.query_results(client, params)
+    def query_results(self, return_results=False, wait_seconds=3600, athena_price_per_tb=5):
+        try:
+            athena_client = self.athena_client
+            params = self.aws_params
+            print('Starting query:\n' + params['query'])
+            ## This function executes the query and returns the query execution ID
+            response_query_execution_id = athena_client.start_query_execution(QueryString=params['query'],
+                                                                       QueryExecutionContext={
+                                                                               'Database': params[
+                                                                                   'database']},
+                                                                       ResultConfiguration={
+                                                                               'OutputLocation': 's3://' +
+                                                                                                 params[
+                                                                                                     'bucket'] + '/' +
+                                                                                                 params[
+                                                                                                     'path']})
 
-def create_ccindex_table(client, params):
-    params['query'] = f"""CREATE EXTERNAL TABLE IF NOT EXISTS ccindex (
-    url_surtkey                   STRING,
-    url                           STRING,
-    url_host_name                 STRING,
-    url_host_tld                  STRING,
-    url_host_2nd_last_part        STRING,
-    url_host_3rd_last_part        STRING,
-    url_host_4th_last_part        STRING,
-    url_host_5th_last_part        STRING,
-    url_host_registry_suffix      STRING,
-    url_host_registered_domain    STRING,
-    url_host_private_suffix       STRING,
-    url_host_private_domain       STRING,
-    url_protocol                  STRING,
-    url_port                      INT,
-    url_path                      STRING,
-    url_query                     STRING,
-    fetch_time                    TIMESTAMP,
-    fetch_status                  SMALLINT,
-    content_digest                STRING,
-    content_mime_type             STRING,
-    content_mime_detected         STRING,
-    content_charset               STRING,
-    content_languages             STRING,
-    warc_filename                 STRING,
-    warc_record_offset            INT,
-    warc_record_length            INT,
-    warc_segment                  STRING)
-    PARTITIONED BY (
-    crawl                         STRING,
-    subset                        STRING)
-    STORED AS parquet
-    LOCATION 's3://commoncrawl/cc-index/table/cc-main/warc/';"""
-    athena_query.query_results(client, params)
+            while wait_seconds > 0:
+                wait_seconds = wait_seconds - 1
+                response_get_query_details = athena_client.get_query_execution(
+                        QueryExecutionId=response_query_execution_id['QueryExecutionId'])
+                status = response_get_query_details['QueryExecution']['Status']['State']
+                execution_time = response_get_query_details['QueryExecution']['Statistics'][
+                    'TotalExecutionTimeInMillis']
+                try:
+                    data_scanned = response_get_query_details['QueryExecution']['Statistics'][
+                        'DataScannedInBytes']
+                except KeyError:
+                    data_scanned = 0
 
-def repair_ccindex_table(client, params):
-    params['query'] = f"""MSCK REPAIR TABLE ccindex;"""
-    athena_query.query_results(client, params)
+                if (status == 'FAILED') or (status == 'CANCELLED'):
+                    failure_reason = response_get_query_details['QueryExecution']['Status'][
+                        'StateChangeReason']
+                    print(failure_reason)
+                    sys.exit(0)
 
-def inner_join(client, params):
-    params['query'] = f"""CREATE TABLE urls_merged_cc AS
-    SELECT url,
-           url_host_name,
-           url_host_registered_domain,
-           warc_filename,
-           warc_record_offset,
-           warc_record_offset + warc_record_length - 1 as warc_record_end,
-           crawl,
-           subset
-    FROM ccindex
-    JOIN url_list ON ccindex.url_host_registered_domain = url_list.websiteaddress
-    WHERE (crawl = 'CC-MAIN-2020-16')
-            -- OR crawl = 'CC-MAIN-2020-24'
-            -- OR crawl = 'CC-MAIN-2020-29' 
-            -- OR crawl = 'CC-MAIN-2020-34'
-            -- OR crawl = 'CC-MAIN-2020-40' 
-            -- OR crawl = 'CC-MAIN-2020-45'
-            -- OR crawl = 'CC-MAIN-2020-50'
-            -- OR crawl = 'CC-MAIN-2021-04'
-            -- OR crawl = 'CC-MAIN-2021-10'
-            -- OR crawl = 'CC-MAIN-2021-17'
-            -- OR crawl = 'CC-MAIN-2021-21'
-            -- OR crawl = 'CC-MAIN-2021-25'
-            -- OR crawl = 'CC-MAIN-2021-31'
-            -- OR crawl = 'CC-MAIN-2021-39'
-            -- OR crawl = 'CC-MAIN-2021-43'
-            -- OR crawl = 'CC-MAIN-2021-49' 
-            -- OR crawl = 'CC-MAIN-2022-05')
-      AND subset = 'warc'"""
-    athena_query.query_results(client, params)
+                elif status == 'SUCCEEDED':
+                    location = response_get_query_details['QueryExecution']['ResultConfiguration'][
+                        'OutputLocation']
+                    print('')
+                    # get size of output file
+                    output_size = self.s3_client.head_object(Bucket=params['bucket'],
+                                                             Key=location.split(params['bucket'] +'/')[1])['ContentLength']
+                    print(f'Query successful! Results are available at {location}. Total size: {output_size*1e-9:.2f}GB.')
 
-create_url_list_table(client, params)
-create_ccindex_table(client, params)
-repair_ccindex_table(client, params)
-inner_join(client, params)
+                    ## Function to get output results
+                    if return_results:
+                        response_query_result = client.get_query_results(
+                                QueryExecutionId=response_query_execution_id['QueryExecutionId'])
+                        result_data = response_query_result['ResultSet']
 
-params['query'] = 'SELECT * FROM ccindex limit 10;'
+                        if len(result_data['Rows']) > 1:
+                            print('Results are available at ' + location)
+                            if return_results:
+                                header = result_data['Rows'][0]
+                                rows = result_data['Rows'][1:]
+                                header = get_var_char_values(header)
+                                result = pd.DataFrame(
+                                        [dict(zip(header, get_var_char_values(row))) for row in rows])
+                                return location, result
 
-location, result = athena_query.query_results(client, params)
+                        else:
+                            print('Result has zero length.')
+                            return location, None
+
+                    else:
+                        return location, None
+
+                else:
+                    print(
+                        f'Time elapsed: {execution_time / 1000}s. Data scanned: {data_scanned * 1e-9:.2f}GB. Total cost: {data_scanned * 1e-12:.2f}$.',
+                        end='\r')
+                    time.sleep(1)
+
+            print('Timed out after 1 hour.')
+            return None, None
+
+        except KeyboardInterrupt:
+            client.stop_query_execution(QueryExecutionId=response_query_execution_id['QueryExecutionId'])
+            print('Keyboard interrupt: Query cancelled.')
+
+    def execute_query(self, query):
+        self.aws_params['query'] = query
+        self.query_results()
+
+    def create_url_list_table(self):
+        query = f"""CREATE EXTERNAL TABLE IF NOT EXISTS url_list (
+        websiteaddress               STRING, 
+        bvdidnumber                  STRING)
+        ROW FORMAT DELIMITED
+        FIELDS TERMINATED BY ','
+        LINES TERMINATED BY '\n'
+        LOCATION '{self.s3path_url_list}'
+        TBLPROPERTIES ('skip.header.line.count' = '1')
+        ;"""
+        self.execute_query(query)
+
+    def create_ccindex_table(self):
+        query = f"""CREATE EXTERNAL TABLE IF NOT EXISTS ccindex (
+        url_surtkey                   STRING,
+        url                           STRING,
+        url_host_name                 STRING,
+        url_host_tld                  STRING,
+        url_host_2nd_last_part        STRING,
+        url_host_3rd_last_part        STRING,
+        url_host_4th_last_part        STRING,
+        url_host_5th_last_part        STRING,
+        url_host_registry_suffix      STRING,
+        url_host_registered_domain    STRING,
+        url_host_private_suffix       STRING,
+        url_host_private_domain       STRING,
+        url_protocol                  STRING,
+        url_port                      INT,
+        url_path                      STRING,
+        url_query                     STRING,
+        fetch_time                    TIMESTAMP,
+        fetch_status                  SMALLINT,
+        content_digest                STRING,
+        content_mime_type             STRING,
+        content_mime_detected         STRING,
+        content_charset               STRING,
+        content_languages             STRING,
+        warc_filename                 STRING,
+        warc_record_offset            INT,
+        warc_record_length            INT,
+        warc_segment                  STRING)
+        PARTITIONED BY (
+        crawl                         STRING,
+        subset                        STRING)
+        STORED AS parquet
+        LOCATION 's3://commoncrawl/cc-index/table/cc-main/warc/';"""
+        self.execute_query(query)
+
+    def repair_ccindex_table(self):
+        query = f"""MSCK REPAIR TABLE ccindex;"""
+        self.execute_query(query)
+
+    def inner_join(self):
+        query = f"""CREATE TABLE urls_merged_cc AS
+        SELECT url,
+               url_host_name,
+               url_host_registered_domain,
+               warc_filename,
+               warc_record_offset,
+               warc_record_offset + warc_record_length - 1 as warc_record_end,
+               crawl,
+               subset
+        FROM ccindex
+        JOIN url_list ON ccindex.url_host_registered_domain = url_list.websiteaddress
+        WHERE crawl = '{self.crawl}'
+          AND subset = 'warc'""" # -- (... OR crawl = 'CC-MAIN-2020-24')
+
+        self.execute_query(query)
+
+    def select_subpages(self):
+        query = f"""select url,
+                    url_host_name,
+                    url_host_registered_domain,
+                    warc_filename,
+                    warc_record_offset,
+                    warc_record_end,
+                    crawl,
+                    subset
+                    from (
+                select url,
+                    url_host_name,
+                    url_host_registered_domain,
+                    warc_filename,
+                    warc_record_offset,
+                    warc_record_end,
+                    crawl,
+                    subset,
+                    row_number() over (partition by url_host_name order by length(url) asc) as subpage_rank 
+                from urls_merged_cc) ranks
+            where subpage_rank <= {self.n_subpages}
+            
+            UNION
+            
+            SELECT url,
+                    url_host_name,
+                    url_host_registered_domain,
+                    warc_filename,
+                    warc_record_offset,
+                    warc_record_end,
+                    crawl,
+                    subset FROM urls_merged_cc
+            WHERE """ + ' OR '.join([f"url LIKE '%{keyword}%'" for keyword in self.url_keywords])
+
+        self.execute_query(query)
+
+    def run_lookup(self):
+        self.create_url_list_table()
+        self.create_ccindex_table()
+        self.repair_ccindex_table()
+        self.inner_join()
+        self.select_subpages()
 
 
 
 
-res = client.start_query_execution(QueryString=params['query'],
-            QueryExecutionContext={'Database': params['database'], 'Catalog': params['catalog']},
-            ResultConfiguration={'OutputLocation': 's3://' + params['bucket'] + '/' + params['path']})
-
-response = client.get_table_metadata(
-    CatalogName=params['catalog'],
-    DatabaseName=params['database'],
-    TableName='ccindex'
-)
-
-results = client.get_query_results(QueryExecutionId=res)
+# awsparams['query'] = 'SELECT * FROM ccindex limit 10;'
+#
+# location, result = athena_query.query_results(client, aws_params)
+#
+#
+#
+#
+# res = client.start_query_execution(QueryString=awsparams['query'],
+#                                    QueryExecutionContext={'Database': awsparams['database'], 'Catalog': awsparams['catalog']},
+#                                    ResultConfiguration={'OutputLocation': 's3://' + awsparams['bucket'] + '/' + awsparams['path']})
+#
+# response = client.get_table_metadata(
+#     CatalogName=awsparams['catalog'],
+#     DatabaseName=awsparams['database'],
+#     TableName='ccindex'
+# )
+#
+# results = client.get_query_results(QueryExecutionId=res)
--- main.py
+++ main.py
@@ -1,82 +1,142 @@
-import pandas as pd
-import os
-import numpy as np
+# first, create an IAM role with full access to S3, Athena, and EC2/Batch
+# Then, configure the session with your AWS credentials by running "aws configure" in your terminal
 import boto3
-from warcio.archiveiterator import ArchiveIterator
-import swifter
-import timeit
-import time
-from io import BytesIO
-from bs4 import BeautifulSoup, SoupStrainer
-import lxml
-import cchardet
-import botocore
-import dask.dataframe as dd
+from athena_lookup import Athena_lookup
+import argparse
+
+# params
+s3path_url_list = 's3://cc-extract/dataprovider_all_months/' # folder where url list is stored, needs to be without 'www.'
+output_bucket = 'cc-extract' # bucket to store the results
+output_path = 'urls_merged_cc' # path in output_bucket to store the results
+crawl='CC-MAIN-2020-16' # crawl name, for list see https://commoncrawl.org/the-data/get-started/
+available_crawls = ['CC-MAIN-2020-16', 'CC-MAIN-2020-24', 'CC-MAIN-2020-29', 'CC-MAIN-2020-34',
+                    'CC-MAIN-2020-40', 'CC-MAIN-2020-45', 'CC-MAIN-2020-50', 'CC-MAIN-2021-04',
+                    'CC-MAIN-2021-10', 'CC-MAIN-2021-17', 'CC-MAIN-2021-21', 'CC-MAIN-2021-25',
+                    'CC-MAIN-2021-31', 'CC-MAIN-2021-39', 'CC-MAIN-2021-43', 'CC-MAIN-2021-49',
+                    'CC-MAIN-2022-05']
+aws_params = {
+    'region': 'us-east-1',
+    'catalog': 'AwsDataCatalog',
+    'database': 'ccindex',
+    'bucket': output_bucket,
+    'path': output_path
+}
+n_subpages = 10 # number of subpages to download per domain
+url_keywords = ['covid', 'corona', 'news', 'press', 'update'] # additionaly include subpages with these keywords in the url
+
+# start up session
+session = boto3.Session()
+
+athena_lookup = Athena_lookup(session, aws_params, s3path_url_list, crawl, n_subpages, url_keywords)
+# athena_lookup.create_url_list_table()
+# athena_lookup.create_ccindex_table()
+# athena_lookup.repair_ccindex_table()
+# athena_lookup.inner_join()
+# athena_lookup.select_subpages()
+athena_lookup.run_lookup()
+
+# def main():
+#     parser = argparse.ArgumentParser(
+#         prog='athena',
+#         usage='athena [--debug] [--execute <statement>] [--output-format <format>] [--schema <schema>]'
+#               ' [--profile <profile>] [--region <region>] [--s3-bucket <bucket>] [--server-side-encryption] [--version]',
+#         description='Download html paragraphs containing keywords from CommonCrawl archive, using an URL list.',
+#     )
+#     parser.add_argument(
+#         '--debug',
+#         action='store_true',
+#         help='enable debug mode'
+#     )
+#
+# if __name__ == '__main__':
+#     main()
 
 
-covid_synonyms = ["covid", "SARS‑CoV‑2", "corona pandemic", "corona",  "covid 19" , "covid-19"
-                  , "corona virus", "coronapandemie", "coronakrise", "SARS CoV 2",
-                  "Wuhan virus", "pandemie", "pandemic", "2019 nCoV", "pandémie",
-                  "pandemia", "Koronapandemie", "Korona", "Coronavirus",
-                  "Coronapandemie", "Wuhan-Virus", "pandémie corona", "Virus de Wuhan", "NCoV 2019",
-                  "pandemia de corona", "coronavirus", "coronapandemia", "cobiçado 19"
-                  , "coronavírus", "Vírus Wuhan", "电晕大流行", "电晕", "冠状病毒", "冠状流感",
-                  "日冕", "武汉病毒", "大流行", "大流行", "2019年nCoV",
-                  "大流行", "कोरोना महामारी" , "कोरोना", "कोविद १ ९", "कोरोना", "ओ और ndemie",
-                  "ओ nakrise", "वुहान वायरस", "ndemie", "महामारी", "コロナパンデミック", "コロナ",
-                  "コロナウイルス", "コロナパンデミ", "コロナクリス", "武漢ウイルス", "パンデミー",
-                  "パンデミック", "パンデミー", "パンデミック", "الاكليل", "جائحة الاكليل",
-                  "مطمع ۱۹", "الفيروس التاجي", "التاج", "السارس", "فيروس ووهان",
-                  "بانديمي", "جائحة", "ncov 2019", "2019 ncov", "пандемия короны",
-                  "корона", "Ковид 19", "коронавирус", "ш и ndemie", "о в nakrise",
-                  "Уханьский вирус", "ndemie", "пандемия", "2019 нКоВ", "ndemia"]
+import pandas as pd
+
+df = pd.read_csv('/Users/Jakob/Downloads/dataprovider_merged_orbis_all_months_unique_urls.csv')
+df.sample(1000).to_csv('/Users/Jakob/Downloads/dataprovider_merged_orbis_all_months_unique_urls_sample.csv')
 
 
-df_sample = pd.read_csv('s3://cc-extract/top10subpages/2022/05/31/8f1d841c-8c64-4c84-85f8-e73c352e156f.csv',
-                 nrows=1000)
-
-
-def fetch_process_warc_records(batch):
-    """Fetch all WARC records defined by filenames and offsets in batch,
-    parse the records and the contained HTML, split the text into words
-    and emit pairs <word, 1>"""
-    batch['input'] = batch.url + '<;>' + batch.warc_filename + '<;>' + batch.warc_record_offset.apply(
-        str) + '<;>' + batch.warc_record_end.apply(str)
-    batch = batch.input.values
-
-    s3client = boto3.client('s3', region_name='us-east-1', use_ssl=False)
-
-    only_paragraphs = SoupStrainer('p')
-
-    results = []
-    for x in batch:
-        url, warc_path, offset, end = x.split('<;>')
-        rangereq = 'bytes={}-{}'.format(offset, end)
-
-        response = s3client.get_object(Bucket='commoncrawl', Key=warc_path, Range=rangereq)
-
-        record_stream = BytesIO(response["Body"].read())
-
-        covid_paragraphs = []
-        for record in ArchiveIterator(record_stream):
-            page = record.content_stream().read()
-
-            soup = BeautifulSoup(page, 'lxml', parse_only=only_paragraphs)
-            text = soup.get_text()
-
-            paragraphs = text.split('\n')
-            covid_paragraphs += [paragraph for paragraph in paragraphs if
-                                 any(ext.casefold() in paragraph.casefold() for ext in covid_synonyms)]
-
-        res = '\n'.join(covid_paragraphs)
-        results.append(res)
-
-    return results
-
-
-start = time.process_time()
-ddf = dd.from_pandas(df, npartitions=os.cpu_count()-2)
-
-result = ddf.map_partitions(fetch_process_warc_records, meta=list)
-result = result.compute(num_workers=os.cpu_count()-2)
-print(time.process_time() - start)
+# import pandas as pd
+# import os
+# import numpy as np
+# import boto3
+# from warcio.archiveiterator import ArchiveIterator
+# import swifter
+# import timeit
+# import time
+# from io import BytesIO
+# from bs4 import BeautifulSoup, SoupStrainer
+# import lxml
+# import cchardet
+# import botocore
+# import dask.dataframe as dd
+#
+#
+# covid_synonyms = ["covid", "SARS‑CoV‑2", "corona pandemic", "corona",  "covid 19" , "covid-19"
+#                   , "corona virus", "coronapandemie", "coronakrise", "SARS CoV 2",
+#                   "Wuhan virus", "pandemie", "pandemic", "2019 nCoV", "pandémie",
+#                   "pandemia", "Koronapandemie", "Korona", "Coronavirus",
+#                   "Coronapandemie", "Wuhan-Virus", "pandémie corona", "Virus de Wuhan", "NCoV 2019",
+#                   "pandemia de corona", "coronavirus", "coronapandemia", "cobiçado 19"
+#                   , "coronavírus", "Vírus Wuhan", "电晕大流行", "电晕", "冠状病毒", "冠状流感",
+#                   "日冕", "武汉病毒", "大流行", "大流行", "2019年nCoV",
+#                   "大流行", "कोरोना महामारी" , "कोरोना", "कोविद १ ९", "कोरोना", "ओ और ndemie",
+#                   "ओ nakrise", "वुहान वायरस", "ndemie", "महामारी", "コロナパンデミック", "コロナ",
+#                   "コロナウイルス", "コロナパンデミ", "コロナクリス", "武漢ウイルス", "パンデミー",
+#                   "パンデミック", "パンデミー", "パンデミック", "الاكليل", "جائحة الاكليل",
+#                   "مطمع ۱۹", "الفيروس التاجي", "التاج", "السارس", "فيروس ووهان",
+#                   "بانديمي", "جائحة", "ncov 2019", "2019 ncov", "пандемия короны",
+#                   "корона", "Ковид 19", "коронавирус", "ш и ndemie", "о в nakrise",
+#                   "Уханьский вирус", "ndemie", "пандемия", "2019 нКоВ", "ndemia"]
+#
+#
+# df_sample = pd.read_csv('s3://cc-extract/top10subpages/2022/05/31/8f1d841c-8c64-4c84-85f8-e73c352e156f.csv',
+#                  nrows=1000)
+#
+#
+# def fetch_process_warc_records(batch):
+#     """Fetch all WARC records defined by filenames and offsets in batch,
+#     parse the records and the contained HTML, split the text into words
+#     and emit pairs <word, 1>"""
+#     batch['input'] = batch.url + '<;>' + batch.warc_filename + '<;>' + batch.warc_record_offset.apply(
+#         str) + '<;>' + batch.warc_record_end.apply(str)
+#     batch = batch.input.values
+#
+#     s3client = boto3.client('s3', region_name='us-east-1', use_ssl=False)
+#
+#     only_paragraphs = SoupStrainer('p')
+#
+#     results = []
+#     for x in batch:
+#         url, warc_path, offset, end = x.split('<;>')
+#         rangereq = 'bytes={}-{}'.format(offset, end)
+#
+#         response = s3client.get_object(Bucket='commoncrawl', Key=warc_path, Range=rangereq)
+#
+#         record_stream = BytesIO(response["Body"].read())
+#
+#         covid_paragraphs = []
+#         for record in ArchiveIterator(record_stream):
+#             page = record.content_stream().read()
+#
+#             soup = BeautifulSoup(page, 'lxml', parse_only=only_paragraphs)
+#             text = soup.get_text()
+#
+#             paragraphs = text.split('\n')
+#             covid_paragraphs += [paragraph for paragraph in paragraphs if
+#                                  any(ext.casefold() in paragraph.casefold() for ext in covid_synonyms)]
+#
+#         res = '\n'.join(covid_paragraphs)
+#         results.append(res)
+#
+#     return results
+#
+#
+# start = time.process_time()
+# ddf = dd.from_pandas(df, npartitions=os.cpu_count()-2)
+#
+# result = ddf.map_partitions(fetch_process_warc_records, meta=list)
+# result = result.compute(num_workers=os.cpu_count()-2)
+# print(time.process_time() - start)
